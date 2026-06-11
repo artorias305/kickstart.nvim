2cd2723c10a53c8ae40749ded2b11956b042d493
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-06-11 16:39:25 -0300

    trying out fff instead of telescope

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -12,4 +12,15 @@ return {
   } },
   { 'chomosuke/typst-preview.nvim', opts = {} },
   { 'alexghergh/nvim-tmux-navigation', opts = {} },
+  {
+    'dmtrKovalenko/fff.nvim',
+    build = function() require('fff.download').download_or_build_binary() end,
+    lazy = false,
+    keys = {
+      { '<leader>sf', function() require('fff').find_files() end, desc = '[S]earch [F]iles' },
+      { '<leader>sg', function() require('fff').live_grep() end, desc = '[S]earch by [G]rep' },
+      { '<leader>sn', function() require('fff').find_files_in_dir '~/.config/nvim' end, desc = '[S]earch [N]eovim files' },
+      { '<leader>sw', function() require('fff').live_grep { query = vim.fn.expand '<cword>' } end, desc = '[S]earch current [W]ord' },
+    },
+  },
 }
