52cfb617d86cc26a792799ecfdba98f49117d2f0
artorias305 <105161738+artorias305@users.noreply.github.com>
2026-07-02 15:46:32 -0300

    cleanup

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,10 +6,13 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  { 'stevearc/oil.nvim', opts = {
-    columns = { 'permissions', 'size', 'mtime' },
-    view_options = { show_hidden = true },
-  } },
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      columns = { 'permissions', 'size', 'mtime' },
+      view_options = { show_hidden = true, is_always_hidden = function(name, bufnr) return name == '..' end },
+    },
+  },
   { 'chomosuke/typst-preview.nvim', opts = {} },
   {
     'ej-shafran/compile-mode.nvim',
